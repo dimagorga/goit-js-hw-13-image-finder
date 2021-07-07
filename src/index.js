6967ee73ae8038ec1645c73111883e74c721d412
Dzmitry Harhun <dimagorga@gmail.com>
2021-07-07 14:35:17 +0300

    добавляет рендер картинок при запросе

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './sass/main.scss';
+import ImagesApiService from './js/apiService';
+import imageCardtemplate from './templates/image-card.hbs'
+var debounce = require('lodash.debounce');
+
+
+const inpitRef = document.querySelector('.search-input')
+const loadMoreBtnRef = document.querySelector('.load-more-btn')
+const cardListRef = document.querySelector('.gallery')
+
+const imagesApiService = new ImagesApiService();
+
+inpitRef.addEventListener('input', debounce(onSearch, 1000))
+loadMoreBtnRef.addEventListener('click', onLoadMore)
+
+
+
+function onSearch(e){
+    e.preventDefault()
+    imagesApiService.query = e.target.value    
+    imagesApiService.resetPage();
+    clearGallery();
+    createImagesList()  
+}
+
+function onLoadMore(){
+    createImagesList()
+    imagesApiService.incrementPage()
+    
+}
+
+function createImagesList(){
+    imagesApiService.fetchImages()
+    .then(hits => {
+        renderImages(hits);
+        }
+    );
+}
+
+function renderImages(hits){
+    cardListRef.insertAdjacentHTML('beforeend', imageCardtemplate(hits))
+}
+function clearGallery(){
+    cardListRef.innerHTML = ''
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,36 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '22395218-1fa4e36600279d8c4f99c77de';
+
+export default class ImagesApiServis {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    
+    fetchImages(){
+        const url = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`
+        return fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            return data.hits            
+        })
+    }
+
+    get query() {
+        return this.searchQuery
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery; 
+    }
+
+    incrementPage(){
+        this.page +=1
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+}
