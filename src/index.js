23b0837f1fa319b869f64a2992752dca362659a7
Dzmitry Harhun <dimagorga@gmail.com>
2021-07-08 11:14:31 +0300

    добавляет стили

--- src/index.js
+++ src/index.js
@@ -7,39 +7,49 @@ var debounce = require('lodash.debounce');
 const inpitRef = document.querySelector('.search-input')
 const loadMoreBtnRef = document.querySelector('.load-more-btn')
 const cardListRef = document.querySelector('.gallery')
+const galeryItemRef = document.querySelector('.gallery-item')
 
 const imagesApiService = new ImagesApiService();
 
-inpitRef.addEventListener('input', debounce(onSearch, 1000))
+inpitRef.addEventListener('input', debounce(onSearch, 500))
 loadMoreBtnRef.addEventListener('click', onLoadMore)
-
+loadMoreBtnRef.setAttribute("disabled", "disabled");
 
 
 function onSearch(e){
     e.preventDefault()
-    imagesApiService.query = e.target.value    
-    imagesApiService.resetPage();
-    clearGallery();
-    createImagesList()  
+    imagesApiService.query = e.target.value       
+    
+    if (e.target.value != '') {     
+        createImagesList(e)  
+        loadMoreBtnRef.removeAttribute("disabled", "disabled");
+    }else{
+        clearGallery();
+        imagesApiService.resetPage();
+        loadMoreBtnRef.setAttribute("disabled", "disabled");
+    }
+    
+}
+
+function createImagesList(e){    
+    imagesApiService.fetchImages()
+        .then(hits => {
+            imagesApiService.incrementPage()
+            renderImages(hits);             
+        }
+    );        
 }
 
 function onLoadMore(){
     createImagesList()
-    imagesApiService.incrementPage()
-    
-}
-
-function createImagesList(){
-    imagesApiService.fetchImages()
-    .then(hits => {
-        renderImages(hits);
-        }
-    );
+         
 }
 
 function renderImages(hits){
     cardListRef.insertAdjacentHTML('beforeend', imageCardtemplate(hits))
 }
+
 function clearGallery(){
     cardListRef.innerHTML = ''
+    
 }
